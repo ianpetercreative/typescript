ed1f639d7ca9268b8e39e8c607aa74e2c96bec0d
ianmacdonald <ianpetercreative@gmail.com>
2024-09-05 16:48:39 -0600

    working with type inferences

--- variables.js
+++ variables.js
@@ -1,24 +1,14 @@
-let movieTitle: string = 'Amadeus'; 
+var movieTitle = 'Amadeus';
 movieTitle = "Arrival";
 // Typescript won't allow you to assign a different type after the type has been declared
 // movieTitle = 8;
 // Typescript won't allow an invalid method
 // movieTitle.round(); 
 // You CAN use a method that works with the data type
-movieTitle.toUpperCase(); 
-
-
-let numCatLives: number = 9;
-numCatLives += 1; 
+movieTitle.toUpperCase();
+var numCatLives = 9;
+numCatLives += 1;
 // numCatLives = "Zero";
-
-let gameOver: boolean = false; 
+var gameOver = false;
 gameOver = true;
 // gameOver = "True"; 
-
-
-
-// Type Inference 
-let tvShow = "The Office";
-
-// Typescript infers the type of the value based on the first instance of that variable. 
