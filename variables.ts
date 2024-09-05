3e2ec8872d1f3ee4448a7020a859dedcbd6d5035
ianmacdonald <ianpetercreative@gmail.com>
2024-09-05 16:31:32 -0600

    working on number and boolean type basics

--- variables.ts
+++ variables.ts
@@ -1,8 +1,17 @@
 let movieTitle: string = 'Amadeus'; 
 movieTitle = "Arrival";
 // Typescript won't allow you to assign a different type after the type has been declared
-movieTitle = 8;
+// movieTitle = 8;
 // Typescript won't allow an invalid method
-movieTitle.round(); 
+// movieTitle.round(); 
 // You CAN use a method that works with the data type
 movieTitle.toUpperCase(); 
+
+
+let numCatLives: number = 9;
+numCatLives += 1; 
+// numCatLives = "Zero";
+
+let gameOver: boolean = false; 
+gameOver = true;
+// gameOver = "True"; 
