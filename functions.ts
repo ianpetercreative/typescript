757949909bafc8ef23768922db2e01cc9235fc9f
ianmacdonald <ianpetercreative@gmail.com>
2024-11-15 15:31:38 -0700

    default parameter values

--- functions.ts
+++ functions.ts
@@ -7,4 +7,15 @@ function greet(person: string){
     return `Hi there, ${person}`;
 }
 
+const doSomething = (person: string, age: number, isFunny: boolean) => {
+    
+};
+
 greet("Ian")
+doSomething("Ian", 30, true)
+// doSomething(true, "Ian", 30)
+
+
+function greetDefault(person: string = "Stranger"){
+    return `Hi there, ${person}`;
+}
