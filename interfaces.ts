33f759682fc6c74bc96be491aa59f4028d07495b
ianpetercreative <ianpetercreative@gmail.com>
2025-01-14 07:47:53 -0700

    Comparing types and interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -48,3 +48,88 @@ const shoes: Product = {
 }
 
 console.log(shoes.applyDiscount(0.4));
+
+
+interface Dog {
+    name: string,
+    age: number
+}
+
+interface Dog {
+    breed: string,
+    bark(): string
+}
+
+const elton: Dog = {
+    name: "Loki",
+    age: 3,
+    breed: "Labrador",
+    bark() {
+        return "WOOF WOOF"
+    }
+}
+
+interface ServiceDog extends Dog {
+    job: "Drug Sniffer" | "Bomb Sniffer" | "Guide Dog"
+}
+
+const chewy: ServiceDog = {
+    name: "Chewy",
+    age: 4.5,
+    breed: "Golden Retriever",
+    bark() {
+        return "Bark!"
+    },
+    job: "Bomb Sniffer"
+}
+
+interface Applicant {
+    name: string
+}
+
+interface Employee {
+    readonly id: number,
+    email: string
+}
+
+interface Engineer extends Applicant, Employee {
+    level: string,
+    languages: string[]
+}
+
+const pierre: Engineer = {
+    name: "Pierre",
+    id: 12345,
+    email: "email@email.com",
+    level: "Senior",
+    languages: ['JS', 'Python']
+}
+
+// KEY DIFFERENCES BETWEEEN TYPES AND INTERFACES
+// Pros to Interfaces:
+// 1. Adding new properties
+// 2. Extending properties (similar to OOP Classes)
+
+// Have to use a Type Alias if we're not describing the shape of an object
+type Color = "Red" | "Blue"
+
+// Types are static/immutable. They can't be added to. 
+type Chicken = {
+    breed: string
+}
+type Chicken = {
+    name: string
+}
+
+// Interfaces use the "extend" keyword similar to object oriented programming with Classes
+// Types can do something similar but the syntax is unusual and not always clean to look at 
+type Name = {
+    name: string;
+}
+type Human = Name & {
+    age: number;
+}
+const jerry: Human = {
+    name: 'Jerry',
+    age: 42
+}
